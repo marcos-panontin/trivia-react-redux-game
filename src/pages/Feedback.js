13ff27f2457c8ae945752adc58c12acc702fac78
Marcos Panontin <amarcospanontin@gmail.com>
2023-06-22 19:27:41 -0300

    feat :tada: : implements Feedback Header

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { generateGravatarURL } from '../services/gravatarFunctions';
 
 class Feedback extends Component {
   render() {
-    const { score, name } = this.props;
+    const { score, name, email } = this.props;
     return (
       <>
 
         <p>Feedback</p>
-        <img data-testid="header-profile-picture" alt="Player Avatar" />
+        <img
+          data-testid="header-profile-picture"
+          alt="Player Avatar"
+          src={ generateGravatarURL(email) }
+        />
         <p data-testid="header-player-name">{name}</p>
         <p data-testid="header-score">{ score }</p>
 
@@ -17,9 +23,16 @@ class Feedback extends Component {
   }
 }
 
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
 const mapStateToProps = (globalState) => ({
   score: globalState.score,
   name: globalState.name,
+  email: globalState.email,
 });
 
 export default connect(mapStateToProps)(Feedback);
