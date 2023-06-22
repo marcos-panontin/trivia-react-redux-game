47f6b91d0038748d4888ffacbed785b61009e558
Marcos Panontin <amarcospanontin@gmail.com>
2023-06-22 19:16:35 -0300

    feat :tada: implements gravatarFunctions.js

--- src/services/gravatarFunctions.js
+++ src/services/gravatarFunctions.js
@@ -1,2 +1,3 @@
-export const generateGravatarURL = (email) => {
-  
+import { md5 } from 'crypto-js';
+
+export const generateGravatarURL = (email) => `https://www.gravatar.com/avatar/${md5(email).toString()}`;
