dd9f73515cb13bad06b668bdb55c0edf6df63efb
Marcos Panontin <amarcospanontin@gmail.com>
2023-06-22 19:11:07 -0300

    build :package: : installs Crypto-JS dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Feedback from './pages/Feedback';
 
 function App() {
   return (
 
     <Switch>
+      <Route exact path="/feedback" component={ Feedback } />
       <Route exact path="/" component={ Login } />
     </Switch>
 
---
+++ src/services/gravatarFunctions.js
@@ -0,0 +1,2 @@
+export const generateGravatarURL = (email) => {
+  
