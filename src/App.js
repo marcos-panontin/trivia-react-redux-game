0483a66d74296f63d47e0e39c030468d23266bc1
Gabriel de Aguiar Fiori <gabriel_afiori@hotmail.com>
2023-06-22 19:55:01 -0300

    Requirement 2 - :heavy_check_mark:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 function App() {
   return (
 
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/game" component={ Game } />
     </Switch>
 
   );
