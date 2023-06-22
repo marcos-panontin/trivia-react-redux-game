95f5cc86c7834393edcdfebabe8b33b1299a7447
Seu nome <yago_bohmcruz@hotmail.com>
2023-06-22 18:16:29 -0300

    INITIAL STATE

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  assertions: 0,
+  score: 0,
+  gravatarEmail: '',
+};
 
 const exampleReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
