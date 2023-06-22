326ad7af6387a94a8fd220be57de55379039e3b8
Marcos Panontin <amarcospanontin@gmail.com>
2023-06-22 20:22:35 -0300

    feat(15):tada: : implements Play Again and Ranking buttons. Creates Ranking page

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,7 +5,7 @@ import { generateGravatarURL } from '../services/gravatarFunctions';
 
 class Feedback extends Component {
   render() {
-    const { score, name, email, assertions } = this.props;
+    const { score, name, email, assertions, history } = this.props;
     const minimumAssertions = 3;
     return (
       <>
@@ -29,6 +29,22 @@ class Feedback extends Component {
         <span data-testid="feedback-total-score">{score}</span>
         <span data-testid="feedback-total-question">{assertions}</span>
 
+        <button
+          data-testid="btn-play-again"
+          onClick={ () => { history.push('/'); } }
+        >
+          Play Again!
+
+        </button>
+
+        <button
+          data-testid="btn-ranking"
+          onClick={ () => { history.push('/ranking'); } }
+        >
+          Ranking
+
+        </button>
+
       </>
     );
   }
@@ -39,13 +55,16 @@ Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
-const mapStateToProps = (globalState) => ({
-  score: globalState.score,
-  assertions: globalState.assertions,
-  name: globalState.name,
-  email: globalState.email,
+const mapStateToProps = ({ player }) => ({
+  score: player.score,
+  assertions: player.assertions,
+  name: player.name,
+  email: player.gravatarEmail,
 });
 
 export default connect(mapStateToProps)(Feedback);
