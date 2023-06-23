4edf11778d8ab437f63c4c577e0f9d4e690e4e09
Gabriel de Aguiar Fiori <gabriel_afiori@hotmail.com>
2023-06-23 17:50:31 -0300

    :fire:

--- src/components/GameSection.jsx
+++ src/components/GameSection.jsx
@@ -4,6 +4,7 @@ import { shuffleArray } from '../services/shuffleArray';
 
 class GameSection extends Component {
   state = {
+    correctAnswerIndex: '',
     buttonClicked: false,
     shuffledAnswers: [],
   };
@@ -17,28 +18,42 @@ class GameSection extends Component {
       } = questionInfo;
 
     const allAnswers = [correctAnswer, ...incorrectAnswers];
-    const shuffledAnswers = shuffleArray(allAnswers);
 
     this.setState({
-      shuffledAnswers,
+      correctAnswerIndex: Math.floor(Math.random() * allAnswers.length) + 1,
     });
   }
+  // componentDidMount() {
+  //   const { questionInfo } = this.props;
+  //   const
+  //     {
+  //       correct_answer: correctAnswer,
+  //       incorrect_answers: incorrectAnswers,
+  //     } = questionInfo;
 
-  componentDidUpdate() {
-    const { questionInfo } = this.props;
-    const
-      {
-        correct_answer: correctAnswer,
-        incorrect_answers: incorrectAnswers,
-      } = questionInfo;
+  //   const allAnswers = [correctAnswer, ...incorrectAnswers];
+  //   const shuffledAnswers = shuffleArray(allAnswers);
 
-    const allAnswers = [correctAnswer, ...incorrectAnswers];
-    const shuffledAnswers = shuffleArray(allAnswers);
+  //   this.setState({
+  //     shuffledAnswers,
+  //   });
+  // }
 
-    this.setState({
-      shuffledAnswers,
-    });
-  }
+  // componentDidUpdate() {
+  //   const { questionInfo } = this.props;
+  //   const
+  //     {
+  //       correct_answer: correctAnswer,
+  //       incorrect_answers: incorrectAnswers,
+  //     } = questionInfo;
+
+  //   const allAnswers = [correctAnswer, ...incorrectAnswers];
+  //   const shuffledAnswers = shuffleArray(allAnswers);
+
+  //   this.setState({
+  //     shuffledAnswers,
+  //   });
+  // }
 
   handleClick = () => {
     this.setState({
@@ -47,26 +62,32 @@ class GameSection extends Component {
   };
 
   render() {
-    const { shuffledAnswers, buttonClicked } = this.state;
     const { questionInfo } = this.props;
     const
       {
+        correct_answer: correctAnswer,
         incorrect_answers: incorrectAnswers,
       } = questionInfo;
+
+    const allAnswers = [correctAnswer, ...incorrectAnswers];
+
+    const { correctAnswerIndex } = this.state;
 
     return (
       <>
         <h2 data-testid="question-category">{questionInfo.category}</h2>
         <p data-testid="question-text">{questionInfo.question}</p>
-        <div data-testid="answer-options">
-          {shuffledAnswers.map((answer, index) => (
+        <div
+          data-testid="answer-options"
+          style={ { display: 'grid' } }
+        >
+          {allAnswers.map((answer, index) => (
             incorrectAnswers.includes(answer)
               ? (
                 <button
                   // style={ buttonClicked ? { border: '3px solid red ' } : { border: '1px solid black ' } }
-                  style={ buttonClicked ? { border: '3px solid red ' } : null }
-
-
+                  // style={ buttonClicked ? { border: '3px solid red ' } : null }
+                  style={ { order: index, border: '3px solid red' } }
                   key={ index }
                   data-testid={ `wrong-answer-${incorrectAnswers.indexOf(answer)}` }
                   type="button"
@@ -78,6 +99,7 @@ class GameSection extends Component {
               : (
                 <button
                   // style={ buttonClicked && { border: '3px solid rgb(6, 240, 15)' } }
+                  style={ { order: correctAnswerIndex } }
                   key={ index }
                   data-testid="correct-answer"
                   type="button"
@@ -98,7 +120,8 @@ GameSection.propTypes = {
     category: PropTypes.string.isRequired,
     question: PropTypes.string.isRequired,
     correct_answer: PropTypes.string.isRequired,
-    incorrect_answers: PropTypes.arrayOf(PropTypes.string).isRequired }).isRequired,
+    incorrect_answers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
 };
 
 export default GameSection;
