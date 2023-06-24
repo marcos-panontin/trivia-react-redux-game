1ec5d146cf196799d093048a99e0319ca587db18
Marcos Panontin <amarcospanontin@gmail.com>
2023-06-24 09:29:38 -0300

    fix :wrench: : Corrects shuffling of answers

--- src/components/GameSection.jsx
+++ src/components/GameSection.jsx
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { shuffleArray } from '../services/shuffleArray';
 
 class GameSection extends Component {
   state = {
     buttonClicked: false,
+    shuffledAnswers: [],
   };
+
+  componentDidMount() {
+    const { questionInfo } = this.props;
+    const
+      {
+        correct_answer: correctAnswer,
+        incorrect_answers: incorrectAnswers,
+      } = questionInfo;
+
+    const allAnswers = [correctAnswer, ...incorrectAnswers];
+    const shuffledAnswers = shuffleArray(allAnswers);
+    this.setState({
+      shuffledAnswers,
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { questionInfo } = this.props;
+    const
+      {
+        correct_answer: correctAnswer,
+        incorrect_answers: incorrectAnswers,
+      } = questionInfo;
+
+    const allAnswers = [correctAnswer, ...incorrectAnswers];
+    const shuffledAnswers = shuffleArray(allAnswers);
+    if (prevProps.questionInfo !== questionInfo) {
+      this.setState({
+        buttonClicked: false,
+        shuffledAnswers,
+      });
+    }
+  }
 
   handleClick = () => {
     this.setState({
@@ -14,15 +49,16 @@ class GameSection extends Component {
   };
 
   render() {
-    const { questionInfo, correctAnswerIndex } = this.props;
-    const { buttonClicked } = this.state;
+    const { buttonClicked, shuffledAnswers } = this.state;
+
+    const { questionInfo } = this.props;
+    // const { buttonClicked } = this.state;
     const
       {
-        correct_answer: correctAnswer,
         incorrect_answers: incorrectAnswers,
       } = questionInfo;
 
-    const allAnswers = [correctAnswer, ...incorrectAnswers];
+    // const allAnswers = [correctAnswer, ...incorrectAnswers];
 
     return (
       <>
@@ -32,13 +68,12 @@ class GameSection extends Component {
           data-testid="answer-options"
           style={ { display: 'grid' } }
         >
-          {allAnswers.map((answer, index) => (
+          {shuffledAnswers.map((answer, index) => (
             incorrectAnswers.includes(answer)
               ? (
                 <button
-                  // style={ buttonClicked ? { border: '3px solid red ' } : { border: '1px solid black ' } }
                   className={ buttonClicked ? 'red' : null }
-                  style={ { order: incorrectAnswers.indexOf(answer) } }
+                  // style={ { order: incorrectAnswers.indexOf(answer) } }
                   key={ index }
                   data-testid={ `wrong-answer-${incorrectAnswers.indexOf(answer)}` }
                   type="button"
@@ -49,8 +84,7 @@ class GameSection extends Component {
               )
               : (
                 <button
-                  // style={ buttonClicked && { border: '3px solid rgb(6, 240, 15)' } }
-                  style={ { order: correctAnswerIndex } }
+                  // style={ { order: correctAnswerIndex } }
                   className={ buttonClicked ? 'green' : null }
                   key={ index }
                   data-testid="correct-answer"
@@ -68,7 +102,7 @@ class GameSection extends Component {
 }
 
 GameSection.propTypes = {
-  correctAnswerIndex: PropTypes.number.isRequired,
+  // correctAnswerIndex: PropTypes.number.isRequired,
   questionInfo: PropTypes.shape({
     category: PropTypes.string.isRequired,
     question: PropTypes.string.isRequired,
