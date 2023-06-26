278139003e47a219165d66caeb32ca5425bb3376
Seu nome <yago_bohmcruz@hotmail.com>
2023-06-26 15:59:26 -0300

    Req 18 e 19 :wrench:

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { generateGravatarURL } from '../services/gravatarFunctions';
 
 class Ranking extends Component {
-  handleRedirect() {
-    const { history } = this.props;
-    history.push('/');
-  }
-
   render() {
+    const { score, name, history, email } = this.props;
     return (
       <div>
-        <p data-testid="ranking-title">Ranking</p>
+        <h2 data-testid="ranking-title">Ranking</h2>
+        <div>
+          <img
+            alt="Player Avatar"
+            src={ generateGravatarURL(email) }
+          />
+          <h4 data-testid="player-name">
+            Player Name:
+            {' '}
+            {name}
+          </h4>
+          <h4 data-testid="player-score">
+            Player Score:
+            {' '}
+            {score}
+          </h4>
+        </div>
         <button
           data-testid="btn-go-home"
-          onClick={ () => this.handleRedirect() }
+          onClick={ () => history.push('/') }
         >
-          Play Again
+          Go Home
         </button>
       </div>
     );
@@ -34,6 +47,9 @@ Ranking.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  score: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
 };
 
 export default connect(mapStateToProps)(Ranking);
